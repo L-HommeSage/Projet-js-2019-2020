7eaac0a7433ee619ea316fb943a7163e59a299a5
L-HommeSage <L-HommeSage24@users.noreply.github.com>
2019-10-22 15:51:49 +0200

    blindage payin payout

--- watermelon/src/Account/Account.js
+++ watermelon/src/Account/Account.js
@@ -13,7 +13,7 @@ class Account extends Component {
                 JSON.parse(localStorage.getItem("wallets")).map((index) => {
                     if (localStorage.getItem("user_log") == index.user_id) {
                         return index.balance;
-                    } 
+                    }
                 }),
         };
 
@@ -31,80 +31,106 @@ class Account extends Component {
         this.setState({ payout: event.target.value });
     }
     handleSubmit1(event) {
-        event.preventDefault();
 
-        let wallets = JSON.parse(localStorage.getItem("wallets"));
+        let bool = true;
+
+        JSON.parse(localStorage.getItem("cards")).map((index) => {
+            if (localStorage.getItem("user_log") == index.user_id) {
+                bool = false;
+            }
+        });
+        if (bool == false) {
+            let wallets = JSON.parse(localStorage.getItem("wallets"));
         wallets[parseInt(localStorage.getItem("user_log") - 1)].balance = parseInt(wallets[localStorage.getItem("user_log") - 1].balance) + parseInt(this.state.payin);
         localStorage.setItem("wallets", JSON.stringify(wallets));
-
-        this.setState({ balance: wallets[parseInt(localStorage.getItem("user_log") - 1)].balance });
-    }
-
-    handleSubmit2(event) {
+        
+        this.setState({ balance: wallets[parseInt(localStorage.getItem("user_log") - 1)].balance })
+            
+        }
+        else{
+            alert("Vous n avez pas de cartes");
+        }
         event.preventDefault();
-        let wallets = JSON.parse(localStorage.getItem("wallets"));
-        wallets[parseInt(localStorage.getItem("user_log") - 1)].balance = parseInt(wallets[localStorage.getItem("user_log") - 1].balance) - parseInt(this.state.payout);
-        localStorage.setItem("wallets", JSON.stringify(wallets));
-
-
-        this.setState({ balance: wallets[parseInt(localStorage.getItem("user_log") - 1)].balance });
     }
     
-    render() {
-        return (
 
-            <div className='first'>
-                <div className='panel1'>
-                    <div className='amount'>
-                        <h1> Balance : {this.state.balance}  euros.</h1>
-                    </div>
+handleSubmit2(event) {
+    let bool = true;
 
-                    <div className='cards'>
-                        {JSON.parse(localStorage.getItem("cards")).map((index) => {
-                            if (localStorage.getItem("user_log") == index.user_id) {
-                                return <div className="cardbox">
-                                    <div className="blackbar">
-                                        </div>
-                                        <div className="Card">
-                                            <Card id={index.id} four={"#### #### #### " + index.last_four} brand={index.brand} expired_at={index.expired_at} />
-                                        </div>
-                                    </div>
-                            }
-                        })}
-                        <div className="cardbox">
-                            <Link to='/addCard'>
-                                <Button outline color="success" onClick={this.add_Card}>
-                                    Ajouter une nouvelle carte
-                                </Button>
-                            </Link>
-                        </div>
-                    </div>
+        JSON.parse(localStorage.getItem("cards")).map((index) => {
+            if (localStorage.getItem("user_log") == index.user_id) {
+                bool = false;
+            }
+        });
+        if (bool == false) {
+            let wallets = JSON.parse(localStorage.getItem("wallets"));
+        wallets[parseInt(localStorage.getItem("user_log") - 1)].balance = parseInt(wallets[localStorage.getItem("user_log") - 1].balance) - parseInt(this.state.payout);
+        localStorage.setItem("wallets", JSON.stringify(wallets));
+        
+        this.setState({ balance: wallets[parseInt(localStorage.getItem("user_log") - 1)].balance })
+            
+        }
+        else{
+            alert("Vous n avez pas de cartes");
+        }
+        event.preventDefault();
+}
+
+render() {
+    return (
+
+        <div className='first'>
+            <div className='panel1'>
+                <div className='amount'>
+                    <h1> Balance : {this.state.balance}  euros.</h1>
                 </div>
 
-                <div className='panel2'>
-                    <div className='box' onSubmit={this.handleSubmit1}>
-                        <Form>
-                            <h1>Payin</h1>
-                            <Input type="text" name="payin" id="de" placeholder="Montant du payin. . ." value={this.state.payin} onChange={this.handleChange1} />
-                            <br></br>
-                            <Button outline color="success">Submit</Button>
-                        </Form>
-                    </div>
-                    <br></br>
-                    <div className='box' onSubmit={this.handleSubmit2} >
-                        <Form >
-                            <h1>payout</h1>
-                            <Input type="text" name="payout" id="re" placeholder="Montant du payout. . ." value={this.state.payout} onChange={this.handleChange2} />
-                            <br></br>
-                            <Button outline color="success">Submit</Button>
-                        </Form>
+                <div className='cards'>
+                    {JSON.parse(localStorage.getItem("cards")).map((index) => {
+                        if (localStorage.getItem("user_log") == index.user_id) {
+                            return <div className="cardbox">
+                                <div className="blackbar">
+                                </div>
+                                <div className="Card">
+                                    <Card id={index.id} four={"#### #### #### " + index.last_four} brand={index.brand} expired_at={index.expired_at} />
+                                </div>
+                            </div>
+                        }
+                    })}
+                    <div className="cardbox">
+                        <Link to='/addCard'>
+                            <Button outline color="success" onClick={this.add_Card}>
+                                Ajouter une nouvelle carte
+                                </Button>
+                        </Link>
                     </div>
                 </div>
             </div>
-            
 
-        );
-    }
+            <div className='panel2'>
+                <div className='box' onSubmit={this.handleSubmit1}>
+                    <Form>
+                        <h1>Payin</h1>
+                        <Input type="text" name="payin" id="de" placeholder="Montant du payin. . ." value={this.state.payin} onChange={this.handleChange1} />
+                        <br></br>
+                        <Button outline color="success">Submit</Button>
+                    </Form>
+                </div>
+                <br></br>
+                <div className='box' onSubmit={this.handleSubmit2} >
+                    <Form >
+                        <h1>payout</h1>
+                        <Input type="text" name="payout" id="re" placeholder="Montant du payout. . ." value={this.state.payout} onChange={this.handleChange2} />
+                        <br></br>
+                        <Button outline color="success">Submit</Button>
+                    </Form>
+                </div>
+            </div>
+        </div>
+
+
+    );
+}
 }
 
 export default Account;
